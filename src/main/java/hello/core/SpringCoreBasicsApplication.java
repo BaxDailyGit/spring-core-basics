508de674a9579e3838ebe4e55be6be5b9468f33a
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-16 23:57:48 +0900

    init

---
+++ src/main/java/hello/core/SpringCoreBasicsApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreBasicsApplication.class, args);
+    }
+
+}
